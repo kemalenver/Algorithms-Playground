4e7ad137ec09b8c7d07e37a5750d218dad8dc715
kemal enver <kemalenver@ip-192-168-67-148.eu-west-1.compute.internal>
2016-02-09 16:37:38 +0000

    Implementation of merge sort

--- Algorithms.playground/Contents.swift
+++ Algorithms.playground/Contents.swift
@@ -71,16 +71,101 @@ func insertionSort<T: Comparable>(inout array: [T]) {
 }
 
 
+
+/*:
+3. Merge Sort
+Has O(n log n) time complexity
+*/
+
+func merge<T: Comparable>(inout array: [T], p : Int, q: Int, r: Int) {
+    
+    var k = p
+    var i = 0
+    var j = 0
+    
+    var lowHalf = [T]()
+    var highHalf = [T]()
+    
+    while k <= q {
+        
+        lowHalf.append(array[k])
+        
+        k+=1
+    }
+    
+    while k <= r {
+        
+        highHalf.append(array[k])
+        
+        k+=1
+    }
+    
+    k = p
+    
+    // Copy while both arrays have elements
+    while i < lowHalf.count && j < highHalf.count  {
+        
+        if lowHalf[i] < highHalf[j] {
+            
+            array[k] = lowHalf[i]
+            i++
+        }
+        else {
+            
+            array[k] = highHalf[j]
+            j++
+        }
+        
+        k++
+    }
+    
+    // When one array is empty, copy the rest
+    
+    while i < lowHalf.count {
+        
+        array[k] = lowHalf[i]
+        i++
+        k++
+    }
+    
+    while j < highHalf.count {
+        
+        array[k] = highHalf[j]
+        j++
+        k++
+    }
+}
+
+
+func mergeSort<T: Comparable>(inout array: [T], p: Int, r: Int) {
+    
+    if p < r {
+        
+        let q = (p + r) / 2
+        
+        mergeSort(&array, p: p, r: q)
+        mergeSort(&array, p: q + 1, r: r)
+        
+        merge(&array, p: p, q: q, r: r)
+    }
+}
+
+
+
+// Tests!
+
 var array1 = [2, 3, 16, 123, 1, 34535, 12, 654, 2, -3, 0x10]
 selectionSort(&array1)
 
 var array2 = [2, 3, 16, 123, 1, 34535, 12, 654, 2, -3, 0x10]
 insertionSort(&array2)
 
+var array3 = [2, 3, 16, 123, 1, 34535, 12, 654, 2, -3, 0x10]
+mergeSort(&array3, p: 0, r: array3.count-1)
 
-//swapValues(&unsortedArray, indexA: 0, indexB: 2)
-//
-//indexOfMin(unsortedArray, startIndex: 4)
+
+
+
 
 
 
